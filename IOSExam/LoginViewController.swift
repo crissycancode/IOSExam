a9e23a74f7257de148f962e977514b02e1324890
Criselda Aguilar <criselda.aguilar@gmail.com>
2023-08-28 02:00:12 +0800

    - Set the placeholder for textfields programatically - Added TextField Delegate with functions for: only accept numbers on `mobileNumberTextField`, remove placeholder text when the user tap the textfield for the first time, dismiss the keyboard when the `return` key is tapped - Set the border style for the textfields

--- IOSExam/LoginViewController.swift
+++ IOSExam/LoginViewController.swift
@@ -26,7 +26,52 @@ class LoginViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setBorderStyle(mobileNumberTextField)
+        setBorderStyle(mpinTextField)
+        
+        mobileNumberTextField.delegate = self
+        mpinTextField.delegate = self
+        mobileNumberTextField.placeholder = "Enter Mobile Number"
+        mpinTextField.placeholder = "Enter MPIN"
     }
+    
+    
+    /// Set the textfield boarders to only have bottom border
+    func setBorderStyle(_ textField: UITextField) {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0,
+                                  y: textField.frame.height + 2.0,
+                                  width: textField.frame.width,
+                                  height: 1.5)
+        bottomLine.backgroundColor = UIColor.lightGray.cgColor
+        textField.borderStyle = .none
+        textField.layer.addSublayer(bottomLine)
+    }
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    
+    /// Only accept numbers for the mobileNumberTextField
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if textField == mobileNumberTextField {
+            let allowedCharacterSet = CharacterSet(charactersIn: "0123456789")
+            let replacementStringIsValid = string.rangeOfCharacter(from: allowedCharacterSet.inverted) == nil
+            return replacementStringIsValid
+        }
+        return true
+    }
+    
+    /// Remove placeholder text on first tap
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == textField.placeholder {
+            textField.text = ""
+        }
+    }
+    
+    /// Dismiss keyboard when `return` is tapped
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
